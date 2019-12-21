517804f71bed5cc4e8a2a167ff6aadd89e119876
lawiraharlan <lawiraharlan@gmail.com>
2019-12-21 22:05:35 +0700

    alan

---
+++ app/src/main/java/com/example/ngebolang/Home_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.ngebolang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_)
+    }
+}
--- app/src/main/java/com/example/ngebolang/loginActivity.kt
+++ app/src/main/java/com/example/ngebolang/loginActivity.kt
@@ -1,13 +1,22 @@
 package com.example.ngebolang
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class loginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener(View.OnClickListener {
+            intent = Intent(this, Home_Activity::class.java)
+            startActivity(intent)
+        })
 
         //alangantitest
     }
