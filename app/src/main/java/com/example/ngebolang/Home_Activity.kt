c70e8a7c745285437cfadddf5fae6b23c2038791
agungr23 <muhammad.agung@student.uty.ac.id>
2019-12-24 15:49:56 +0700

    firebase,showhidepwd

--- app/src/main/java/com/example/ngebolang/Home_Activity.kt
+++ app/src/main/java/com/example/ngebolang/Home_Activity.kt
@@ -8,5 +8,6 @@ class Home_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_)
+
     }
 }
