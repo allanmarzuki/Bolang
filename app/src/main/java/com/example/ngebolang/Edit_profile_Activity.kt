08a92c82c79b6f5a5047cf259ba27a4bfc287fa0
lawiraharlan <lawiraharlan@gmail.com>
2019-12-26 21:11:37 +0700

    alan - editprofile fix

---
+++ app/src/main/java/com/example/ngebolang/Edit_profile_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.ngebolang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Edit_profile_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile_)
+    }
+}
--- app/src/main/java/com/example/ngebolang/Home_Activity.kt
+++ app/src/main/java/com/example/ngebolang/Home_Activity.kt
@@ -1,12 +1,23 @@
 package com.example.ngebolang
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Home_Activity : AppCompatActivity() {
+
+    private lateinit var photo_profil : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_)
+
+        photo_profil = findViewById(R.id.pict_photo_home_user)
+
+        photo_profil.setOnClickListener{
+            val goto_user_page = Intent(this, Edit_profile_Activity::class.java)
+            startActivity(goto_user_page)
+        }
     }
 }
